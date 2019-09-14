749856dfdf6863d6aa9fb045e4fbd35ff984e9d5
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-13 23:35:05 -0400

    Update reverselinkedlist.py

--- reverselinkedlist.py
+++ reverselinkedlist.py
@@ -8,17 +8,29 @@
 # linear time and constant space as we just allocate one node and then rearrange nodes of original linked list
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        result = ListNode(0) #dummy node
-        self.reverseList_(head, result)
-        return result.next
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         result = ListNode(0) #dummy node
+#         self.reverseList_(head, result)
+#         return result.next
     
-    def reverseList_(self, head, result):
-        if not head:
-            return None #function modifies result so we can just return none here to end the loop
+#     def reverseList_(self, head, result):
+#         if not head:
+#             return None #function modifies result so we can just return none here to end the loop
         
-        rest = result.next 
-        result.next = head
-        store = head.next
-        result.next.next = rest
-        self.reverseList_(store, result)
+#         rest = result.next 
+#         result.next = head
+#         store = head.next
+#         result.next.next = rest
+#         self.reverseList_(store, result)
+
+# optimized solution with better recursive solution
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        
+        p = self.reverseList(head.next) # stack level recurses all the way to the end and then returns last element along the way
+        head.next.next = head #next element's.next points to head so that head actually follows head.next
+        head.next = None #last element (head) will point to None
+        return p #return last element as new head
+        
