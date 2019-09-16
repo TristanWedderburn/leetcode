fcfc1ae9cac995e43e64e2a91d3079543d869285
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-15 23:50:33 -0400

    Create setmatrixzeroes.py

---
+++ setmatrixzeroes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        if not matrix:
+            return
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        row = [0]*n
+        col = [0]*m
+
+        for i in range(n):
+            for j in range(m):
+                val = matrix[i][j]
+                if val == 0:
+                    print(i,j)
+                    row[i] = 1
+                    col[j] = 1
+
+        for k in range(n):
+            if row[k] == 1:
+                for l in range(m):
+                    matrix[k][l] = 0
+
+        for a in range(m):
+            if col[a] == 1:
+                for b in range(n):
+                    matrix[b][a] = 0
