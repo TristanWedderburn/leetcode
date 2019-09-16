c65fdf3d69da90b7a2193d9c223a3abdc1f0df93
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-16 10:11:21 -0400

    Update longestsubstringwithoutrepeatingcharacters.py

--- longestsubstringwithoutrepeatingcharacters.py
+++ longestsubstringwithoutrepeatingcharacters.py
@@ -1,24 +1,40 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if not s or len(s)<1:
+#             return 0
+#         if len(s) == 1:
+#             return 1
+        
+#         bank = [0]*128
+#         left=0
+#         right=0
+#         maxSubstring=0
+        
+#         while right < len(s): #update max incrementally
+#             val = s[right]
+#             index = ord(val)
+#             if bank[index] == 0: #if val is not in bank, mark as passed
+#                 maxSubstring = max(maxSubstring, right-left+1)
+#                 bank[index]=1
+#                 right+=1 #check for next char to add
+#             else:
+#                 bank[ord(s[left])]=0 #remove leftmost char from bank
+#                 left+=1 #retry without leftmost char
+#         return maxSubstring
+# can optimize by storing last location in a map to skip left variable to the last known index+1
         if not s or len(s)<1:
             return 0
         if len(s) == 1:
             return 1
-        
+
         bank = [0]*128
         left=0
-        right=0
         maxSubstring=0
-        
-        while right < len(s): #update max incrementally
+
+        for right in range(len(s)): #update max incrementally
             val = s[right]
             index = ord(val)
-            if bank[index] == 0: #if val is not in bank, mark as passed
-                maxSubstring = max(maxSubstring, right-left+1)
-                bank[index]=1
-                right+=1 #check for next char to add
-            else:
-                bank[ord(s[left])]=0 #remove leftmost char from bank
-                left+=1 #retry without leftmost char
+            left = max(bank[index], left)
+            maxSubstring = max(maxSubstring, right - left + 1);
+            bank[index] = right + 1; #store index of variable as we encounter it
         return maxSubstring
-# can optimize by storing last location in a map to skip left variable to the last known index+1
