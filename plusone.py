99054b45a698a202fe8c44ead7f51bb21462d1c8
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-17 16:27:24 -0400

    Create plusone.py

---
+++ plusone.py
@@ -0,0 +1,27 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if not digits:
+          return []
+        
+        val = digits[-1]
+        val+=1
+        digits[-1] = val
+        
+        if val == 10: #if we need to add a carry, pass it down into the array
+            carry = val // 10
+            for i in range(1, len(digits)): #traverse array from the back
+                newVal = digits[len(digits)-i-1]
+                newVal+=carry
+                if newVal == 10: #pass on carry
+                    digits[len(digits)-i-1] = newVal%10
+                    carry = newVal // 10
+                else: #just add carry to number
+                    digits[len(digits)-i-1] = newVal
+                    carry = 0
+                
+        if carry == 1:    #if there is a carry after traversing the whole array, add a new index in the front of the array
+            newDigits = [carry]
+            newDigits.extend(digits)
+            digits = newDigits
+            
+        return digits
