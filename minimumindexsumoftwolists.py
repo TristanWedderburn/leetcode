8e762dd9013862ff54fa2a3d18c181ec941923fd
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-18 12:04:35 -0400

    Update minimumindexsumoftwolists.py

--- minimumindexsumoftwolists.py
+++ minimumindexsumoftwolists.py
@@ -4,16 +4,16 @@ class Solution:
         common = []
         indexSum = len(list1)+len(list2)
         
-        for i,a in enumerate(list1):
-            restaurants[a] = i
+        for i, choices1 in enumerate(list1):
+            restaurants[choices1] = i
         
-        for j,b in enumerate(list2):
-            if b in restaurants:
-                sum = j+restaurants[b]
+        for j,choices2 in enumerate(list2):
+            if choices2 in restaurants:
+                sum = j+restaurants[choices2]
                 if sum < indexSum:
                     indexSum = sum
-                    common = [b]
+                    common = [choices2]
                 elif sum == indexSum:
-                    common.append(b)
+                    common.append(choices2)
                 
         return common
