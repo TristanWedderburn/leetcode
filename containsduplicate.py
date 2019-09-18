85952ce0b32b3e292f87769766ce31e9fcb2faf9
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-18 02:45:12 -0400

    Create containsduplicate.py

---
+++ containsduplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        uniqueSet = set()
+        for num in nums:
+            if num in uniqueSet:
+                return True
+            else:
+                uniqueSet.add(num)
+        return False
