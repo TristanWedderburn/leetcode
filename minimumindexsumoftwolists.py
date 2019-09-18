dd77c831616fdc79622d3641b7b17c6172eb126f
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-09-18 12:04:51 -0400

    Update minimumindexsumoftwolists.py

--- minimumindexsumoftwolists.py
+++ minimumindexsumoftwolists.py
@@ -7,7 +7,7 @@ class Solution:
         for i, choices1 in enumerate(list1):
             restaurants[choices1] = i
         
-        for j,choices2 in enumerate(list2):
+        for j, choices2 in enumerate(list2):
             if choices2 in restaurants:
                 sum = j+restaurants[choices2]
                 if sum < indexSum:
