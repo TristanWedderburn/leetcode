7f84cc285af5f230f0a4872ed775919e1d62cf93
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-10-09 13:17:04 -0400

    Update intersectionOfTwoLinkedLists.py

--- intersectionOfTwoLinkedLists.py
+++ intersectionOfTwoLinkedLists.py
@@ -10,19 +10,32 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        if not headA or not headB:
-            return None
+#         time: O(max(n,m))
+#         space: O(max(n))
+#         if not headA or not headB:
+#             return None
         
-        setA = set()
+#         setA = set()
         
+#         pA = headA
+#         while(pA!=None):
+#             setA.add(pA)
+#             pA=pA.next
+        
+#         pB = headB
+#         while(pB!=None):
+#             if pB in setA:
+#                 return pB
+#             pB=pB.next
+#         return None
+# in order to reduce the space complexity of the solution, we can traverse the linked list twice. 
+# Since both of the linked lists have the same ending, the quicker solution restart the list quicker
+# in order to offset the list indexing and find the intersected nodes.
+# time: O(n+m)
+# space: O(1)
         pA = headA
-        while(pA!=None):
-            setA.add(pA)
-            pA=pA.next
-        
         pB = headB
-        while(pB!=None):
-            if pB in setA:
-                return pB
-            pB=pB.next
-        return None
+        while(pA!=pB):
+            pA = pA.next if pA!=None else headB
+            pB = pB.next if pB!=None else headA
+        return pA
