de1130055f5ae6c0600cb3cb2b0e96e09ca996ef
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-10-15 14:48:33 -0400

    Update convertSortedArrayToBinarySearchTree.py

--- convertSortedArrayToBinarySearchTree.py
+++ convertSortedArrayToBinarySearchTree.py
@@ -14,11 +14,11 @@ class Solution(object):
         if not nums:
             return
         
-        return self.pivot(nums, 0, len(nums))
+        return self.pivot(nums, 0, len(nums)) #create bst by adding left and right nodes using binary search on input array in left and right sub arrays
         
     def pivot(self, nums, low, high):
-        if low >= high:
-            return None
+        if low >= high: #exit when node is a leaf
+            return
         else:
             mid = low + (high-low)//2
             
