186330c5c0bfa0c6c4def9ea63891cb1e7abb1a5
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-10-17 02:55:35 -0400

    Update reverselinkedlist.py

--- reverselinkedlist.py
+++ reverselinkedlist.py
@@ -1,48 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-# add in front of linked list
-# linear time and constant space as we just allocate one node and then rearrange nodes of original linked list
-
-class Solution:
-#     def reverseList(self, head: ListNode) -> ListNode:
-#         result = ListNode(0) #dummy node
-#         self.reverseList_(head, result)
-#         return result.next
-    
-#     def reverseList_(self, head, result):
-#         if not head:
-#             return None #function modifies result so we can just return none here to end the loop
-        
-#         rest = result.next 
-#         result.next = head
-#         store = head.next
-#         result.next.next = rest
-#         self.reverseList_(store, result)
-
-# better recursive solution
-
-#     def reverseList(self, head: ListNode) -> ListNode:
-#         if not head or not head.next:
+# class Solution(object):
+#     def reverseList(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: ListNode
+#         """
+#         if not head or head.next == None:
 #             return head
-        
-#         p = self.reverseList(head.next) # stack level recurses all the way to the end and then returns last element along the way
-#         head.next.next = head #next element's.next points to head so that head actually follows head.next
-#         head.next = None #last element (head) will point to None
-#         return p #return last element as new head
-        
-# iterative solution
-    def reverseList(self, head: ListNode) -> ListNode:
-        prev = None
+#         node = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return node
+    
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """        
         curr = head
+        prev = None
         
-        while(curr):
-            nextNode = curr.next
+        while curr:
+            temp = curr.next
             curr.next = prev
             prev = curr
-            curr = nextNode
-            
+            curr = temp
         return prev
