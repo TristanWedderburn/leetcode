257ae8dfe369782a49d4488f044a3d271c6398f7
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-10-17 03:29:50 -0400

    Update mergetwosortedlists.py

--- mergetwosortedlists.py
+++ mergetwosortedlists.py
@@ -1,44 +1,41 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-#     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-#         result = result_tail = ListNode(0)
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
         
-#         while(l1 and l2):
-#             val = 0
-#             if(l1.val<=l2.val):
-#                 val = l1.val
-#                 l1 = l1.next
-#             else:
-#                 val = l2.val
-#                 l2 = l2.next
-#             result_tail.next = ListNode(val)
-#             result_tail = result_tail.next
+        if l1.val <= l2.val:
+            head = tail = l1
+            l1 = l1.next
+        else:
+            head = tail = l2
+            l2 = l2.next
         
-#         if(l1 or l2):
-#             result_tail.next = l2 if l1 == None else l1
-        
-#         return result.next
-
-# we can optimize solution even more by using constant space and just reassigning the links of all of the nodes using the result_tail node
-    
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        result = result_tail = ListNode(0)
-        
-        while(l1 and l2):
-            if(l1.val<=l2.val):
-                result_tail.next = l1
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
                 l1 = l1.next
             else:
-                result_tail.next = l2
+                tail.next = l2
                 l2 = l2.next
-            result_tail = result_tail.next
+            tail = tail.next
         
-        if(l1 or l2):
-            result_tail.next = l2 if l1 == None else l1
+        if l2:
+            tail.next = l2
+        elif l1:
+            tail.next = l1
+
+        return head
         
-        return result.next
