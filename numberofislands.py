d6399688116cc7eeb490e3edfb9b1e36ea611c9b
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-10-22 04:18:04 -0400

    Update numberofislands.py

--- numberofislands.py
+++ numberofislands.py
@@ -1,53 +1,48 @@
-class Solution:
-#     dfs solution is prone to recursion depth error
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         if not grid:
-#             return 0 #0 islands found
+from collections import deque
+class Solution(object):
+    def __init__(self):
+        self.grid = [[]]
         
-#         count = 0
-        
-#         for i in range(len(grid)):
-#             for j in range(len(grid[0])):
-#                 if grid[i][j] == '1':
-#                     self.dfs(grid, i, j)
-#                     count+=1 
-                    
-#         return count
-    
-#     def dfs(self, grid, i, j):
-#         if i<0 or j<0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-# #             if i or j are out of bounds or if the value isn't 1, return
-#             return
-#         else:
-#             grid[i][j] = '0' #turn island passed into water for other dfs calls
-#             self.dfs(grid, i+1, j)
-#             self.dfs(grid, i-1, j)
-#             self.dfs(grid, i, j+1)
-#             self.dfs(grid, i, j-1)
-
-# bfs solution
     def numIslands(self, grid):
-        if not grid or not grid[0]:
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
             return 0
+        
+        self.grid = grid
+        islands = 0
+        
+        n = len(self.grid)
+        m = len(self.grid[0])
+        
+        for i in range(n):
+            for j in range(m):
+                if self.grid[i][j]=='1':
+                    islands+=1 #found new island
+                    self.grid[i][j]='0' #visit
+                    
+                    island = deque()
+                    island.append((i,j))
+                    
+                    #bfs
+                    while island:
+                        next = island.popleft()
+                        ni = next[0]
+                        nj = next[1]
+                        
+                        if ni+1<n and self.grid[ni+1][nj]=='1':
+                            island.append((ni+1,nj))
+                            self.grid[ni+1][nj]='0'
+                        if ni-1>=0 and self.grid[ni-1][nj]=='1':
+                            island.append((ni-1,nj))
+                            self.grid[ni-1][nj]='0'
+                        if nj+1<m and self.grid[ni][nj+1]=='1':
+                            island.append((ni,nj+1))
+                            self.grid[ni][nj+1]='0'
+                        if nj-1>=0 and self.grid[ni][nj-1]=='1':
+                            island.append((ni,nj-1))
+                            self.grid[ni][nj-1]='0'
 
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.bfs(grid, i, j)
-                    count += 1
-        return count
-
-    def bfs(self, grid, i, j):
-        queue = [] #could use collections.deque() 
-        queue.append((i, j))
-        grid[i][j] = '0'
-        while queue:
-            directions = [(0,1), (0,-1), (-1,0), (1,0)]
-            i, j = queue.pop(0)
-            for d in directions:
-                nextI = i + d[0]
-                nextJ = j + d[1]    
-                if nextI >= 0 and nextI < len(grid) and nextJ >= 0 and nextJ< len(grid[0]) and grid[nextI][nextJ] == '1':
-                    queue.append((nextI, nextJ))
-                    grid[nextI][nextJ] = '0'
+        return islands
