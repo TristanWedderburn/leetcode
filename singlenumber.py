47b64f67ee9a7c0a99627b9c7e6cdd637b8ff3a1
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-11-07 16:57:59 -0500

    Update singlenumber.py

--- singlenumber.py
+++ singlenumber.py
@@ -1,7 +1,30 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        single = 0
+#   binary optimization
+#     def singleNumber(self, nums: List[int]) -> int:
+#         single = 0
         
-        for num in nums:
-            single^=num
-        return single
+#         for num in nums:
+#             single^=num
+#         return single
+
+#       O(n) time and O(1) space solution
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        nums.sort()
+        i, singleNum = 0,0
+        
+        while i < len(nums):
+            if (i+1 < len(nums) and nums[i+1] == nums[i]):
+                while (i+1 < len(nums) and nums[i+1] == nums[i]):
+                    i+=1
+                i+=1 
+                continue
+            singleNum = nums[i]
+            i+=1
+            
+        return singleNum
+        
