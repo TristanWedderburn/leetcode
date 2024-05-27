293e383e929549201d37fc26f2fcec7ab042405d
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-05-27 19:34:15 -0400

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,19 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums or (len(nums) ==1 and nums[0]!=target):
-            return -1
-        
+        # left and right pointers
+        # if greater than mid, left is mid + 1
+        # if less than mid, right is mid - 1
+        # if value found, return true
+        # stop at left > right
+
         left = 0
-        right = len(nums)
-        
-        while(left <= right):
-            mid = left + (right-left)//2
-            val = nums[mid]
-            if val == target:
+        right = len(nums) - 1
+
+        while (left <= right):
+            mid = (left + right) // 2 # checking middle of binary search
+            
+            if (nums[mid] == target):
                 return mid
-            elif val > target:
-                right = mid-1
-            else:
-                left = mid+1
-                    
+            elif (nums[mid] > target): # left tree
+                right = mid - 1
+                continue
+            else: # right tree
+                left = mid + 1
+                continue
+
         return -1
+ 
