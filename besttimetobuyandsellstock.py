bda159d0c2a7e758c827f8398c1bf11735dfa1d7
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-05-27 20:28:44 -0400

    Update besttimetobuyandsellstock.py

--- besttimetobuyandsellstock.py
+++ besttimetobuyandsellstock.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit, buy_index, sell_index = 0, 0, 1
+        min_buy_price = prices[0]
+        max_sell_price = 0
+        max_profit = 0
 
-        while(sell_index < len(prices)): # move sell to find best price to sell at
-            if (prices[buy_index] < prices[sell_index]): # if profitable, check
-                max_profit = max(max_profit, prices[sell_index] - prices[buy_index])
-            else: # move the buy index to lower price
-                buy_index = sell_index
-            sell_index+=1 # check next sell date
-
+        for i in range(len(prices)):
+            if (prices[i] < min_buy_price): # new price to buy at
+                min_buy_price = prices[i]
+            elif prices[i] - min_buy_price > max_profit: # if more profitable, set
+                max_sell_price = prices[i]
+                max_profit = prices[i] - min_buy_price
         return max_profit
