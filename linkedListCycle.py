17da8959f2fdbc29808f917860c74e8a6e966c6e
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-05-29 19:37:52 -0400

    Update linkedListCycle.py

--- linkedListCycle.py
+++ linkedListCycle.py
@@ -1,46 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-# O(n) time and space
-class Solution(object):
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        seen = set()
-        
-        while head:
-            if head in seen:
-                return True
-            seen.add(head)
-            head = head.next
-        return False
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # fast and slow pointers
+        # use a set and iterate until reach end
 
-# O(n) time, O(1) space
-# class Solution(object):
-#     def hasCycle(self, head):
-#         """
-#         :type head: ListNode
-#         :rtype: bool
-#         """
-#         if not head:
-#             return False
-        
-#         slow = head
-#         fast = head.next
-        
-#         while(slow and fast):
-#             if slow == fast:
-#                 return True
-            
-#             if fast.next and fast.next.next:
-#                 fast = fast.next.next
-#                 slow = slow.next
-#             else:
-#                 return False
-#         return False
-        
+        if not head:
+            return False
+
+        # slow moves by 1, fast moves by 2
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
