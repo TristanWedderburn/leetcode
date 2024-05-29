e29b30051038079815982be9078bae27bfff0568
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-05-29 02:04:53 -0400

    Update reverselinkedlist.py

--- reverselinkedlist.py
+++ reverselinkedlist.py
@@ -1,34 +1,18 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # use pointer to track what the last node was before overwriting it
 
-# class Solution(object):
-#     def reverseList(self, head):
-#         """
-#         :type head: ListNode
-#         :rtype: ListNode
-#         """
-#         if not head or head.next == None:
-#             return head
-#         node = self.reverseList(head.next)
-#         head.next.next = head
-#         head.next = None
-#         return node
-    
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """        
-        curr = head
-        prev = None
+        prev: Optional[ListNode] = None
+
+        while (head): # continue iterating until reach end of linked list
+            tmp = head.next # store old .next
+            head.next = prev # set new .next
+            prev = head # mark as seen
+            head = tmp # process old .next
         
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
         return prev
