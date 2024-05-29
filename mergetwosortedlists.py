7ffa985b330eb75287df746ce0bab17622a62e0f
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-05-29 18:57:42 -0400

    Update mergetwosortedlists.py

--- mergetwosortedlists.py
+++ mergetwosortedlists.py
@@ -1,39 +1,57 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # two pointers
+        # one pointer used as the head of the new linked list, head
+        # one pointer to traverse, curr
 
-# O(n) time and O(1) space
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        
-        if l1.val <= l2.val:
-            head = tail = l1
-            l1 = l1.next
+        # compare current val of each list head
+        # smaller is head
+        # advance list
+        # head, curr
+
+        # loop compare current vals
+        # smaller is curr.next
+        # advance list
+        # advance to curr.next
+
+        # return head
+
+        # handle empty lists
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+
+        head = curr = None
+
+        # figure out where to start merged list
+        if list1.val <= list2.val:
+            head = curr = list1
+            list1 = list1.next
         else:
-            head = tail = l2
-            l2 = l2.next
-        
-        while l1 and l2:
-            if l1.val <= l2.val:
-                tail.next = l1
-                l1 = l1.next
+            head = curr = list2
+            list2 = list2.next
+
+        # iterate on each list
+        while (list1 and list2):
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+                curr = curr.next
             else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
+                curr.next = list2
+                list2 = list2.next
+                curr = curr.next
         
-        tail.next = l2 if not l1 else l1
+        # handle one list finishing before the other
+        if list1:
+            curr.next = list1
+        elif list2:
+            curr.next = list2
 
         return head
-        
