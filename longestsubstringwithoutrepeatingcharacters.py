0c19df7205da90cf58c24f102096fd4ca36d2b5d
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-06-11 23:02:24 -0400

    Update longestsubstringwithoutrepeatingcharacters.py

--- longestsubstringwithoutrepeatingcharacters.py
+++ longestsubstringwithoutrepeatingcharacters.py
@@ -1,5 +1,5 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
 #         if not s or len(s)<1:
 #             return 0
 #         if len(s) == 1:
@@ -24,18 +24,39 @@ class Solution:
 
 # can optimize by storing last location in a map to skip left variable to the last known index+1
 
-        if not s or len(s)<1:
-            return 0
-        if len(s) == 1:
-            return 1
+        # if not s or len(s)<1:
+        #     return 0
+        # if len(s) == 1:
+        #     return 1
 
-        bank = [0]*128
-        left=0
-        maxSubstring=0
+        # bank = [0]*128
+        # left=0
+        # maxSubstring=0
 
-        for right in range(len(s)): #update max incrementally
-            index = ord(s[right])
-            left = max(bank[index], left)
-            maxSubstring = max(maxSubstring, right - left + 1);
-            bank[index] = right + 1; #store index of variable as we encounter it
-        return maxSubstring
+        # for right in range(len(s)): #update max incrementally
+        #     index = ord(s[right])
+        #     left = max(bank[index], left)
+        #     maxSubstring = max(maxSubstring, right - left + 1);
+        #     bank[index] = right + 1; #store index of variable as we encounter it
+        # return maxSubstring
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # sliding window on string
+        # map to maintain char to last index        
+        last_seen = {}
+        left, right, max_len = 0, 0, 0
+
+        while right < len(s):
+            c = s[right]
+
+            if c not in last_seen or last_seen[c] < left: # add key to map if not seen or if the repeated character is not in the substring anymore
+                last_seen[c] = right
+                max_len = max(max_len, right - left + 1) # check max
+            else: # seen
+                left = last_seen[c] + 1 # move left to last seen + 1
+                last_seen[c] = right 
+            
+            right+=1
+        
+        return max_len
