c64827131b1272cd903e4bc5969fcc7f6d75ca45
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2024-12-16 00:19:36 -0500

    Update groupAnagrams.py

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -4,15 +4,11 @@ class Solution:
         # iterate through strings and add sorted string as key
         # if key already in there, append to list
         # return all values
-        anagram_map = {}
+        anagram_map = defaultdict(list)
 
         for s in strs:
             # sort 
             sorted_s = ''.join(sorted(s))
-
-            if sorted_s not in anagram_map:
-                anagram_map[sorted_s] = []
-            
             anagram_map[sorted_s].append(s)
         
         return [l for l in anagram_map.values()]
